c14996e6dd86a5dbadd0d46d0f502b4f77b903c0
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2022-07-21 14:01:30 +0300

    hesabım sayfasında bir takım sekmeleri yoruma aldım olur da vakit kalırsa ekleriz yeniden

--- lib/screens/musteri/hesap_view.dart
+++ lib/screens/musteri/hesap_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:idea_ecommerce_app/screens/musteri/adreslerim.dart';
 
 class KullaniciHesabiView extends StatelessWidget {
   const KullaniciHesabiView({Key? key}) : super(key: key);
@@ -34,20 +35,25 @@ class KullaniciHesabiView extends StatelessWidget {
               },
               child: myCard(Icons.shopping_basket, 'Siparişlerim'),
             ),
-            GestureDetector(
+            /* GestureDetector(
               onTap: () {
                 print('cüzdanıma gidecek');
               },
               child: myCard(Icons.wallet_giftcard, 'Cüzdanım'),
-            ),
-            GestureDetector(
+            ), */
+            /* GestureDetector(
               onTap: () {
                 print('favorilerime gidecek');
               },
               child: myCard(Icons.favorite, 'Favorilerim'),
-            ),
+            ), */
             GestureDetector(
               onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => adreslerim(),
+                    ));
                 print('Adreslerim sayfasına gidecek');
               },
               child: myCard(Icons.location_pin, 'Adreslerim'),
@@ -58,12 +64,12 @@ class KullaniciHesabiView extends StatelessWidget {
               },
               child: myCard(Icons.stars, 'Değerlendirmelerim'),
             ),
-            GestureDetector(
+            /* GestureDetector(
               onTap: () {
                 print('Müşteri Hizmetleri sayfasına gidecek');
               },
               child: myCard(Icons.headphones, 'Müşteri Hizmetleri'),
-            ),
+            ), */
             GestureDetector(
               onTap: () {
                 print('ayarlarıma gidecek');
